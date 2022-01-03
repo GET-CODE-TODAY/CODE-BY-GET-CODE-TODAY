df8bf7b8f91283bef228acab40472fdc3868b3a9
GET-CODE-TODAY <96962238+GET-CODE-TODAY@users.noreply.github.com>
2022-01-02 22:49:06 -0600

    BOOK-write a book in .js
    This code is basic and ment for cheap uses. I don't recommend using it for any really good books or big companies! 
    
    Have fun coding!

---
+++ BOOK-write a book in.js
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>Book name here</title>
+    <script src="main.js"></script>
+  </head>
+  <body>
+    <h1>Bookname,Chapter here</h1> 
+    <!-- This is a comment -->By:
+<!-- This is a comment -->"your name here" <h1>________________________________________________________________</h1>
+      <script>
+      var num = 5;
+          document.write('<br>"Books text here"');
+      </script>
+  </body>
+</html>
